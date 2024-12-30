75b3de967e5d7c08fdd601f5de1bbbd3ac51ccf2
maheshpandule8 <maheshpandule8@gmail.com>
2024-12-30 13:47:16 +0530

    Initial Repository Mahesh

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("i am Mahesh");
+import React from 'react'
+
+
+export const index = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ mygit.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const mygit = () => {
+    return (
+        <div>
+            <h1>mygit  Mahesh</h1>
+        </div>
+    )
+}
+
+export default mygit
